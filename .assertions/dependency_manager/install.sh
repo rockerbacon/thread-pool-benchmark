0d02bc8ff98126782f83d4feecc92e0324bebba7
rockerbacon <vas.vitor.santos@gmail.com>
2020-03-09 10:25:08 -0300

    Updated assertions

--- .assertions/dependency_manager/install.sh
+++ .assertions/dependency_manager/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-apt-package/install.sh g++
-apt-package/install.sh cmake
-apt-package/install.sh make
-git/install.sh https://github.com/progschj/ThreadPool.git 9a42ec1329f259a5f4881a291db1dcb8f2ad9040 false "." "." "rm example.cpp"
-git/install.sh https://github.com/rockerbacon/parallel-tools.git v1.0.2 false "src/objs" "src/objs" ""
-git/install.sh https://github.com/rockerbacon/cpp-benchmark.git v1.0.5 false "src/objs" "src/objs" ""
-git/install.sh https://github.com/rockerbacon/stopwatch.git v2.1.4 false "src/objs" "src/objs" ""
-git/install.sh https://github.com/mtrebi/thread-pool.git 1c80f3abfda89afcacfb39a8e892aeb8eed0c299 false "include" "include" ""
+source apt-package/install.sh g++
+source apt-package/install.sh cmake
+source apt-package/install.sh make
+source git/install.sh progschj/ThreadPool.git --version 9a42ec1329f259a5f4881a291db1dcb8f2ad9040 --objs-dir "." --include-dir "." --before-linking "rm example.cpp" --alias progschj
+source git/install.sh rockerbacon/parallel-tools.git --version v1.3 --alias rockerbacon
+source git/install.sh rockerbacon/cpp-benchmark.git --version v1.0.5
+source git/install.sh rockerbacon/stopwatch.git --version v2.1.4
+source git/install.sh mtrebi/thread-pool.git --version 1c80f3abfda89afcacfb39a8e892aeb8eed0c299 --objs-dir "include" --include-dir "include" --alias mtrebi
